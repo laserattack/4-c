7ac9cd6161fb86370eff81bad6dfb61ca3f5b05c
serr <sc7227484@gmail.com>
2026-03-02 00:03:17 +0300

    vars in macro start with _

--- sources/c4arr.h
+++ sources/c4arr.h
@@ -15,83 +15,83 @@ typedef struct {
 #define c4arrlen(arr)    ((arr)? (c4arrheader(arr))->len: 0)
 #define c4arrcap(arr)    ((arr)? (c4arrheader(arr))->cap: 0)
 
-#define c4arrfree(arr)                                                           \
-    do {                                                                         \
-        if (!(arr)) break;                                                       \
-        free(c4arrheader(arr));                                                  \
-        (arr) = NULL;                                                            \
+#define c4arrfree(arr)                                                            \
+    do {                                                                          \
+        if (!(arr)) break;                                                        \
+        free(c4arrheader(arr));                                                   \
+        (arr) = NULL;                                                             \
     } while(0)
 
-#define c4arrclear(arr)                                                          \
-    do {                                                                         \
-        if (!(arr)) break;                                                       \
-        (c4arrheader(arr))->len = 0;                                             \
+#define c4arrclear(arr)                                                           \
+    do {                                                                          \
+        if (!(arr)) break;                                                        \
+        (c4arrheader(arr))->len = 0;                                              \
     } while(0)
 
-#define c4arrpush(arr, el)                                                       \
-    do {                                                                         \
-        C4ArrHeader *h;                                                          \
-        if (!(arr)) {                                                            \
-            h = malloc(sizeof(C4ArrHeader) + C4_ARR_INIT_CAP * sizeof(*(arr)));  \
-            if (!h) {                                                            \
-                fprintf(stderr, "c4arr alloc err\n");                            \
-                exit(1);                                                         \
-            }                                                                    \
-            h->cap = C4_ARR_INIT_CAP;                                            \
-            h->len = 0;                                                          \
-            (arr) = (void *)h + sizeof(C4ArrHeader);                             \
-        }                                                                        \
-        h = c4arrheader(arr);                                                    \
-        h->len++;                                                                \
-        if (h->len > h->cap) {                                                   \
-            h->cap *= 2;                                                         \
-            h = realloc(h, sizeof(C4ArrHeader) + h->cap * sizeof(*(arr)));       \
-            if (!h) {                                                            \
-                fprintf(stderr, "c4arr realloc err\n");                          \
-                exit(1);                                                         \
-            }                                                                    \
-            (arr) = (void *)h + sizeof(C4ArrHeader);                             \
-        }                                                                        \
-        (arr)[h->len-1] = el;                                                    \
+#define c4arrpush(arr, el)                                                        \
+    do {                                                                          \
+        C4ArrHeader *_h;                                                          \
+        if (!(arr)) {                                                             \
+            _h = malloc(sizeof(C4ArrHeader) + C4_ARR_INIT_CAP * sizeof(*(arr)));  \
+            if (!_h) {                                                            \
+                fprintf(stderr, "c4arr alloc err\n");                             \
+                exit(1);                                                          \
+            }                                                                     \
+            _h->cap = C4_ARR_INIT_CAP;                                            \
+            _h->len = 0;                                                          \
+            (arr) = (void *)_h + sizeof(C4ArrHeader);                             \
+        }                                                                         \
+        _h = c4arrheader(arr);                                                    \
+        _h->len++;                                                                \
+        if (_h->len > _h->cap) {                                                  \
+            _h->cap *= 2;                                                         \
+            _h = realloc(_h, sizeof(C4ArrHeader) + _h->cap * sizeof(*(arr)));     \
+            if (!_h) {                                                            \
+                fprintf(stderr, "c4arr realloc err\n");                           \
+                exit(1);                                                          \
+            }                                                                     \
+            (arr) = (void *)_h + sizeof(C4ArrHeader);                             \
+        }                                                                         \
+        (arr)[_h->len-1] = el;                                                    \
     } while(0)
 
-#define c4arrinsert(arr, idx, el)                                                \
-    do {                                                                         \
-        if (!(arr)) break;                                                       \
-        if ((idx) <= c4arrlen(arr) && (idx) >= 0) {                              \
-            c4arrpush(arr, el);                                                  \
-            for (int c4i = c4arrlen(arr) - 1; c4i > (idx); c4i--)                \
-                (arr)[c4i] = (arr)[c4i - 1];                                     \
-            (arr)[idx] = el;                                                     \
-        }                                                                        \
+#define c4arrinsert(arr, idx, el)                                                 \
+    do {                                                                          \
+        if (!(arr)) break;                                                        \
+        if ((idx) <= c4arrlen(arr) && (idx) >= 0) {                               \
+            c4arrpush(arr, el);                                                   \
+            for (int _i = c4arrlen(arr) - 1; _i > (idx); _i--)                    \
+                (arr)[_i] = (arr)[_i - 1];                                        \
+            (arr)[idx] = el;                                                      \
+        }                                                                         \
     } while(0)
 
-#define c4arrremove(arr, idx)                                                    \
-    do {                                                                         \
-        if (!(arr)) break;                                                       \
-        if ((idx) < c4arrlen(arr) && (idx) >= 0) {                               \
-            for (int c4i = (idx); c4i < c4arrlen(arr) - 1; c4i++)                \
-                (arr)[c4i] = (arr)[c4i + 1];                                     \
-            c4arrheader(arr)->len--;                                             \
-        }                                                                        \
+#define c4arrremove(arr, idx)                                                     \
+    do {                                                                          \
+        if (!(arr)) break;                                                        \
+        if ((idx) < c4arrlen(arr) && (idx) >= 0) {                                \
+            for (int _i = (idx); _i < c4arrlen(arr) - 1; _i++)                    \
+                (arr)[_i] = (arr)[_i + 1];                                        \
+            c4arrheader(arr)->len--;                                              \
+        }                                                                         \
     } while(0)
 
-#define c4arrshrink(arr)                                                         \
-    do {                                                                         \
-        if (!(arr)) break;                                                       \
-        C4ArrHeader *h;                                                          \
-        h = c4arrheader(arr);                                                    \
-        h->cap = h->len;                                                         \
-        if (!h->cap) {                                                           \
-            c4arrfree(arr);                                                      \
-        } else {                                                                 \
-            h = realloc(h, sizeof(C4ArrHeader) + h->cap * sizeof(*(arr)));       \
-            if (!h) {                                                            \
-                fprintf(stderr, "c4arr realloc err\n");                          \
-                exit(1);                                                         \
-            }                                                                    \
-            (arr) = (void *)h + sizeof(C4ArrHeader);                             \
-        }                                                                        \
+#define c4arrshrink(arr)                                                          \
+    do {                                                                          \
+        if (!(arr)) break;                                                        \
+        C4ArrHeader *_h;                                                          \
+        _h = c4arrheader(arr);                                                    \
+        _h->cap = _h->len;                                                        \
+        if (!_h->cap) {                                                           \
+            c4arrfree(arr);                                                       \
+        } else {                                                                  \
+            _h = realloc(_h, sizeof(C4ArrHeader) + _h->cap * sizeof(*(arr)));     \
+            if (!_h) {                                                            \
+                fprintf(stderr, "c4arr realloc err\n");                           \
+                exit(1);                                                          \
+            }                                                                     \
+            (arr) = (void *)_h + sizeof(C4ArrHeader);                             \
+        }                                                                         \
     } while(0)
 
 #endif
